75b687dad797ab6a29ba3f4aa71005b977af9d68
Vinhkt2005 <luuvinh357@gmail.com>
2025-07-30 21:16:00 +0700

    Add student portal with profile and course views

---
+++ ViewModels/StudentHomeViewModel.cs
@@ -0,0 +1,10 @@
+namespace Web_SIMS.ViewModels
+{
+    public class StudentHomeViewModel
+    {
+        public int CurrentCourses { get; set; }
+        public decimal CurrentGpa { get; set; }
+        public int TotalCredits { get; set; }
+        public int CompletedCourses { get; set; }
+    }
+}
